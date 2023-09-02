0f03656e7187934bcf0373deaec81106bee03cf2
Jockio <Jockio@163.com>
2023-09-02 11:53:18 +0800

    Update XiaoHuaJia.js

--- Script/XiaoHuaJia.js
+++ Script/XiaoHuaJia.js
@@ -1,2 +1,2 @@
-body = $response.body.replace(/vip":0/g, 'vip":1').replace(/vip_expire_time":0/g, 'vip_expire_time":4100726622');
+body = $response.body.replace(/vip":0/g, 'vip":1').replace(/vip_expire_time":0/g, 'vip_expire_time":4100726622').replace(/ai_book_count":0/g, 'ai_book_count":20').replace(/"_s":\{[^}]+\}/g, "\"tt\":\{\}");
 $done({body});
